f4aeca0476f161f0dc2c6de4459ed347bc024aa9
David Berto <david.berto@wingman.pt>
2020-08-25 15:31:07 +0100

    Added workspace with viper project and SwiftLint

---
+++ VIPER/VIPER/Application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VIPER
+//
+//  Created by David Manuel da Costa Berto on 25/08/2020.
+//  Copyright © 2020 David Manuel da Costa Berto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
